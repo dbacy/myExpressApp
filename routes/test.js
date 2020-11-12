36c7f2a85ed16dd6e943aa57f5329ec2888a7a94
dbacy <69469697+dbacy@users.noreply.github.com>
2020-11-12 13:38:00 -0600

    Update

--- routes/test.js
+++ routes/test.js
@@ -12,15 +12,66 @@ router.post('/math', function (req, res, next) {
   let num1 = req.body.number1 | 0;     // fastest way to convert a text number to a number
   let num2 = req.body.number2 | 0;     // OR the number with 0
   let op = req.body.operator;
+  let outBase = req.body.output
+
+  let result
+  function fibonacci(num){
+    var a = 1, b = 0, temp;
+  
+    while (num >= 0){
+      temp = a;
+      a = a + b;
+      b = temp;
+      num--;
+    }
+  
+    return b;
+  }
+function factorialize(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0) 
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+  }
 
   //  you now have two numbers and a math operator
   //  you do the math!
+  switch(op) {
+  case "+" :
+    result= num1 + num2
+    break
+  case "-" :
+    result= num1 - num2
+    break
+  case "/" :
+    result= num1 / num2
+    break
+  case "*" :
+    result= num1 * num2
+    break
+  case "%" :
+    result= num1 % num2
+    break
+  // case "!" :
+  //   result=  
+  //   break
+  // case "f" :
+  //   result=  
+  //   break
+  case "^" :
+    result= num1 ^ num2      
+    break 
+  }
 
   res.render('test', {
     number1: num1, 
     number2: num2, 
     operator: op,
-    mathResult: num1 + op + num2
+    output: outBase,
+    mathResult: result
   });
 });
 
@@ -48,5 +99,43 @@ router.post('/find', function (req, res, next) {
     findResult: result
   });
 });
+
+//  compare two numbers and return a message about how they compare to each other
+router.post('/compare', function (req, res, next) {
+  let num3 = req.body.number3 | 0
+  let num4 = req.body.number4 | 0
+
+  let result
+  if (num3 < num4)
+  result = num3 + " is less than " + num4
+  else if (num4 < num3)
+  result = num4 + " is less than " + num3
+  else
+  result = num3 + " and " + num4 + " are the same"
+  
+  //  return compareResult to the browser
+  res.render('test', {
+      number3: num3,
+      number4: num4,
+      compareResult: result
+  });
+});
+
+//  compare two numbers and return a message about how they compare to each other
+router.post('/substring', function (req, res, next) {
+  let text = req.body.text1
+  let num5 = req.body.number5 | 0
+  let num6 = req.body.number6 | 0
+  let substring = text.substring(num5,num6)
+  //  return compareResult to the browser
+  res.render('test', {
+      text1:text,
+      number5: num5,
+      number6: num6,
+      substringResult: substring
+  });
+});
+
+
 
 module.exports = router;
