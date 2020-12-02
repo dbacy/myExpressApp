f2b3ed1e5f1cf44d937b1ac77326b38c7044af14
dbacy <69469697+dbacy@users.noreply.github.com>
2020-12-02 14:54:59 -0600

    Update

--- routes/test.js
+++ routes/test.js
@@ -136,6 +136,66 @@ router.post('/substring', function (req, res, next) {
   });
 });
 
+router.post('/string', function (req, res, next) {
+  let text = req.body.text2;
+  let upper = text.toUpperCase()
+  let length = text.length
+  let words = text.split(" ") 
+  let countOfWords = words.length
+  let result = `There are ${countOfWords} words in the text. Your word upper cases is ${upper}. Your string is ${length} long. `
+  
+  //  return stringResult to the browser
+  res.render('test', {
+    text2: text, 
+    
+    stringResult: result
+  });
+});
+
+// Array Test
+// Convert Test String to an array of words. Enter a number (Return Word#)
+// Return the word at this position in the array of words
+router.post('/arrays', function (req, res, next) {
+  let text = req.body.text4;
+  let num7 = req.body.number7 | 0
+  let convertToArray = text.split(" ")
+  let wordZero = convertToArray[num7]
+  
+  //  return arraysResult to the browser
+  res.render('test', {
+    text4: text, 
+    number7: num7,
+    arraysResult: wordZero
+  });
+});
+
+router.post('/temp', function (req, res, next) {
+  let temp = req.body.temp | 0
+  let result
+
+  if (temp <= 85 )
+      result = "Hypothermia call 911"
+  else if(temp > 85 && temp <= 94)
+      result = "Just cold , put on a jacket"
+  else if(temp > 94 && temp <= 99)
+      result = "Normal - no action needed"
+  else if(temp > 99 && temp <= 101)
+      result = "Slight fever - take an asprin"
+  else if(temp >101 && temp <= 105)
+      result = "Get to a hospital"
+  else
+      result = "RIP"
+
+  //  return tempResult to the browser
+  res.render('test', {
+    temp: temp,
+    tempResult: result
+  });
+});
+
+
+
+
 
 
 module.exports = router;
